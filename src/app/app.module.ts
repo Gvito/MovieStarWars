b068233df559bb4b1282c9c9c6b1bdf2530002c8
gino <ginovito@live.fr>
2019-03-21 16:58:49 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PeopleComponent } from './people/people.component';
+import { PlanetsComponent } from './planets/planets.component';
+import { StarshipsComponent } from './starships/starships.component';
+import { PeopleDetailComponent } from './people-detail/people-detail.component';
+import { PlanetsDetailComponent } from './planets-detail/planets-detail.component';
+import { StarshipsDetailComponent } from './starships-detail/starships-detail.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PeopleComponent,
+    PlanetsComponent,
+    StarshipsComponent,
+    PeopleDetailComponent,
+    PlanetsDetailComponent,
+    StarshipsDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
